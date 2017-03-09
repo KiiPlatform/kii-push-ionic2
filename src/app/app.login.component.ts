15f937fb744921db36968f8cb47389190c7fff99
satoshi <satoshi.kumano@kii.com>
2017-03-09 17:10:29 +0900

    Implementing screen transition.

--- src/app/app.login.component.ts
+++ src/app/app.login.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { MainPage } from './app.main.component';
+import { NavController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'app.login.html'
 })
 
 export class LoginPage {
-
+  constructor(public navCtrl: NavController) {
+  }
+  login() {
+    console.log("Login clicked")
+    this.navCtrl.push(MainPage);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { LoginPage } from './app.login.component';
+import { MainPage } from './app.main.component';
 
 @NgModule({
   declarations: [
     MyApp,
     LoginPage,
+    MainPage,
   ],
   imports: [
     IonicModule.forRoot(MyApp)
@@ -14,7 +16,8 @@ import { LoginPage } from './app.login.component';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    LoginPage
+    LoginPage,
+    MainPage,
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
