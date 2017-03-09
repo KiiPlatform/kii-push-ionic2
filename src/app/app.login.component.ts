65cbc4c4923e8ddac83e4c31b9317240a9cb373d
satoshi <satoshi.kumano@kii.com>
2017-03-09 18:20:41 +0900

    Implement login.

--- src/app/app.login.component.ts
+++ src/app/app.login.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { MainPage } from './app.main.component';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   templateUrl: 'app.login.html'
 })
 
 export class LoginPage {
-  constructor(public navCtrl: NavController) {
+  username: string
+  password: string
+  constructor(private navCtrl: NavController,
+              private alertCtrl: AlertController) {
   }
   login() {
-    this.navCtrl.push(MainPage);
+    KiiUser.authenticate(this.username, this.password).then(
+        (user:KiiUser) => {
+          this.navCtrl.push(MainPage);
+        },
+    ).catch((error:Error) => {
+      let alert = this.alertCtrl.create({
+        title: 'Failed to Login',
+        message: error.message,
+        buttons: ['OK']
+      });
+      alert.present();
+    })
   }
 }
