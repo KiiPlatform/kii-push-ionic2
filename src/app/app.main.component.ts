b2885cdec84c0b576f6d340d488cde7746d82ed0
satoshi <satoshi.kumano@kii.com>
2017-03-10 14:12:03 +0900

    Adding push installation.

--- src/app/app.main.component.ts
+++ src/app/app.main.component.ts
@@ -8,6 +8,7 @@ import { NavController } from 'ionic-angular';
 
 export class MainPage {
   constructor(private navCtrl: NavController) {
+      this.installPush()
   }
   logout() {
     KiiUser.logOut()
@@ -16,4 +17,34 @@ export class MainPage {
   userID() : string {
     return KiiUser.getCurrentUser().getID()
   }
+
+  installPush() {
+      let push = PushNotification.init({
+          android: {
+              senderID: "12345679"
+          },
+          ios: {
+              alert: "true",
+              badge: true,
+              sound: 'false'
+          }
+      });
+
+      push.on('registration', (data) => {
+          console.log(data.registrationId);
+      });
+
+      push.on('notification', (data) => {
+          console.log(data.message);
+          console.log(data.title);
+          console.log(data.count);
+          console.log(data.sound);
+          console.log(data.image);
+          console.log(data.additionalData);
+      });
+      push.on('error', (e) => {
+          console.log(e.message);
+      });
+  }
+
 }
